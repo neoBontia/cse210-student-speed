3581d5105749c8a4ddbdef54d585948778df5465
MasterBeans <jul21002@byui.edu>
2021-10-30 17:29:31 +0800

    score.py completed

---
+++ speed/game/word.py
@@ -0,0 +1,15 @@
+import random
+from game import constants
+from game.actor import Actor
+from game.point import Point
+
+
+class Word:
+    def __init__(self):
+        self._word_bank = []
+
+    def reset(self):
+        pass
+
+    def get_points(self):
+        pass
